d99ce17c25b1b3e36eeac3d9db123c0e57670679
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-09-14 18:20:18 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -144,4 +144,14 @@ class Database:
         return (await self.db.command("dbstats"))['dataSize']
 
 
-X
+db = Database(DATABASE_URI, DATABASE_NAME)
+
+shortz = shortzy.Shortzy(SHORTENER_API, "shareus.in")
+async def get_shortlink(link):
+    if SHORTENER_API:
+        if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
+            return await shortz.get_quick_link(link)
+        else:
+            return await shortz.convert(link, silently_fail=False)
+    return link
+
