ff0e6b58fd72b1ed065d94fcbc719d380073facb
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-09-27 16:03:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,7 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Movie Save In Gallery', url='https://youtu.be/WUSusf8s2gM') ] ] ),
+reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('How To Save Movie In Gallery', url='https://youtu.be/WUSusf8s2gM') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
     await asyncio.sleep(600) 
