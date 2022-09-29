85a3e006e52ec75bfe9940b358d36b91347197f5
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-09-29 23:58:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -617,7 +617,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     elif query.data == 'fp':
-        await query.answer("⚠️ Information ⚠️\n\nᴀꜰᴛᴇʀ 2 ᴍɪɴɪᴜᴛ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy ᴅᴇʟᴇᴛᴇ\n\nIf you do not see the requested\n\nmovie / series file, look at the next page\n\n©Bot Owner and Admin", show_alert=True),
+        await query.answer("⚠️ Information ⚠️ \n\n 🤖 Bot is Not Open source project. ", show_alert=True),
     await query.answer('Piracy Is Crime')
 
 
