cc4ef6ce8786c4dfec64e4d4df58eb7cbbc593eb
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-09-30 12:36:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -733,16 +733,10 @@ async def auto_filter(client, msg, spoll=False):
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
-     m=await message.reply_sticker("CAACAgUAAxkBAAIIzWK9wmZSiQN5wYLauCvQtC7a1O0rAAITCAACp9HIVA6oKX23md8bHgQ")
-        await asyncio.sleep(1)
-        await m.delete()
         fuk = await message.reply_photo(photo="https://te.legra.ph/file/b0caab9e5a4f577842731.jpg", caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(20)
         await fuk.delete()
         await msg.delete()
-      
-    if spoll:
-        await msg.message.delete()
 
 async def advantage_spell_chok(msg):
     query = re.sub(
