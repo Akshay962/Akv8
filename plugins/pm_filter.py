51e74dbfeb6cb2499e9d9629137b45c1afb4ce80
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-09-30 20:04:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -617,7 +617,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     elif query.data == 'fp':
-        await query.answer(" is Not Open source project. ", show_alert=True),
+        await query.answer("Movies Format ‼️\n\nPushpa ☑️\nPushpa Hindi ☑️\nPushpa Hindi dubbed movie ❎\n\nWeb Series Format ‼️\n\n Money Heist ☑️\nMoney Heist S01E07 ☑️\nMoney Heist All Season ❎ ", show_alert=True),
     await query.answer('Piracy Is Crime')
 
 async def auto_filter(client, msg, spoll=False):
